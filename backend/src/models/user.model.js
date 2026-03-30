8be2a475441866eaaea8c5343e07454796fd6e28
WolfY <david.skliarov.dev@gmail.com>
2026-03-30 17:47:21 +0400

    feat: add user schema and model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		email: { type: String, unique: true },
+		fullName: { type: String, required: true },
+		password: { type: String, required: true, minlength: 6 },
+		profilePic: { type: String, default: "" },
+	},
+	{ timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
