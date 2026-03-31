3c7bd5c3877cbb2050f68b67c6b706b00bd109b7
WolfY <david.skliarov.dev@gmail.com>
2026-03-31 15:52:24 +0400

    feat: implement login function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,8 +49,33 @@ const signup = async (req, res) => {
 	}
 };
 
-const login = (req, res) => {
-	res.end("Login route");
+const login = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const user = await User.findOne({ email });
+
+		if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+		console.log(isPasswordCorrect);
+
+		if (!isPasswordCorrect)
+			return res.status(400).json({ message: "Invalid credentials" });
+
+		generateToken(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			email: user.email,
+			profilePic: user.profilePic,
+		});
+	} catch (error) {
+		console.log(`Error in login controller ${error.message}`);
+		res.status(500).json({ message: "Internal server error" });
+	}
 };
 
 const logout = (req, res) => {
