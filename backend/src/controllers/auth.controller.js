541197a12326481425ece05ba03ecac70e6095a8
WolfY <david.skliarov.dev@gmail.com>
2026-03-31 17:21:21 +0400

    feat: implement updateProfile and checkAuth functions

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 const signup = async (req, res) => {
 	const { email, fullName, password, profilePic } = req.body;
@@ -88,4 +89,37 @@ const logout = (req, res) => {
 	}
 };
 
-export { login, logout, signup };
+const updateProfile = async (req, res) => {
+	try {
+		const { profilePic } = req.body;
+		const userId = req.user._id;
+
+		if (!profilePic)
+			return res.status(400).json({ message: "Profile pic is required" });
+
+		const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+		const updatedUser = await User.findByIdAndUpdate(
+			userId,
+			{
+				profilePic: uploadResponse.secure_url,
+			},
+			{ new: true },
+		);
+
+		res.status(200).json(updatedUser);
+	} catch (error) {
+		console.log(`Error in updateProfile controller ${error.message}`);
+	}
+};
+
+const checkAuth = (req, res) => {
+	try {
+		res.status(200).json(req.user);
+	} catch (error) {
+		console.log(`Error in checkAuth controller ${error.message}`);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
+};
+
+export { checkAuth, login, logout, signup, updateProfile };
