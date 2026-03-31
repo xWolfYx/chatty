4f536f9067c8f55e2171579dbef489372f873842
WolfY <david.skliarov.dev@gmail.com>
2026-03-31 14:55:00 +0400

    feat: implement saving of a user in signup function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,10 @@
 import bcrypt from "bcryptjs";
-import User from "../models/user.model";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
 
 const signup = async (req, res) => {
 	const { email, fullName, password, profilePic } = req.body;
+
 	try {
 		if (!email || !fullName)
 			res.status(400).json({ message: "All fields must be filled" });
@@ -20,15 +22,30 @@ const signup = async (req, res) => {
 
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
+		console.log(hashedPassword);
 
-		const newUser = new User({ fullName, email, hashedPassword, profilePic });
+		const newUser = new User({
+			fullName,
+			email,
+			password: hashedPassword,
+			profilePic,
+		});
 
 		if (newUser) {
+			generateToken(newUser._id, res);
+			await newUser.save();
+			res.status(201).json({
+				_id: newUser._id,
+				fullName: newUser.fullName,
+				email: newUser.email,
+				profilePic: newUser.profilePic,
+			});
 		} else {
 			res.status(400).json("Invalid user data");
 		}
 	} catch (error) {
-		console.log(`Signup error: ${error.message}`);
+		console.log(`Error in signup controller: ${error.message}`);
+		res.status(500).json({ message: "Internal server error" });
 	}
 };
 
