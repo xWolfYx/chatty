36df2b5fa8ce9e4ffda7e720631043f4c964feaf
WolfY <david.skliarov.dev@gmail.com>
2026-04-01 17:21:43 +0400

    feat: add signup component base state

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+
 export default function SignupPage() {
-	return <div>Signup Page</div>;
+	// const [showPassword, setShowPassword] = useState(false);
+	// const [formData, setFormData] = useState({
+	// 	fullName: "",
+	// 	email: "",
+	// 	password: "",
+	// });
+
+	// const { isSigningUp, signUp } = useAuthStore();
+
+	// const validateForm = () => {};
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// };
+
+	return <div className="grid lg:grid-cols-2 min-h-screen"></div>;
 }
