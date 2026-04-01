d47d094a7757965e5fa88a475c1bfcbf56582fae
WolfY <david.skliarov.dev@gmail.com>
2026-04-01 15:31:34 +0400

    feat: implement App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,52 @@
+import { Loader2 } from "lucide-react";
+import { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import SignupPage from "./pages/SignupPage";
+import { useAuthStore } from "./store/useAuthStore";
+
 export default function App() {
-	return <div>Text</div>;
+	const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+	useEffect(() => {
+		checkAuth();
+	}, [checkAuth]);
+
+	console.log(`Auth User:\n${authUser}`);
+
+	if (isCheckingAuth && !authUser)
+		return (
+			<div className="flex justify-center items-center h-screen">
+				<Loader2 className="size-10 animate-spin" />
+			</div>
+		);
+
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+				/>
+				<Route
+					path="/signup"
+					element={!authUser ? <SignupPage /> : <Navigate to={"/"} />}
+				/>
+				<Route
+					path="/login"
+					element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+				/>
+				<Route
+					path="/profile"
+					element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}
+				/>
+				<Route path="/settings" element={<SettingsPage />} />
+			</Routes>
+		</div>
+	);
 }
