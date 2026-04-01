057e43b4eba37fd0071688e4ff2d1dd852a61876
WolfY <david.skliarov.dev@gmail.com>
2026-04-01 15:18:51 +0400

    feat: implement checkAuth function

---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import type { AuthState, User } from "../lib/types";
+
+const useAuthStore = create<AuthState>((set) => ({
+	authUser: null,
+	isSigningUp: false,
+	isLoggingIn: false,
+	isUpdatingProfile: false,
+	isCheckingAuth: true,
+
+	checkAuth: async () => {
+		try {
+			const res = await axiosInstance.get<User>("/auth/check");
+			set({ authUser: res.data });
+		} catch (error: unknown) {
+			if (error instanceof Error) console.log(error.message);
+			set({ authUser: null });
+		} finally {
+			set({ isCheckingAuth: false });
+		}
+	},
+}));
+
+export { useAuthStore };
