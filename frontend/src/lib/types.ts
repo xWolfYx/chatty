c8fa9d4ee9bdcdd296495c42e865b2877f6aea4c
WolfY <david.skliarov.dev@gmail.com>
2026-04-01 15:19:08 +0400

    feat: add types for User and AuthState

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,18 @@
+export type User = {
+	_id: string;
+	email: string;
+	fullName: string;
+	password: string;
+	profilePic: string;
+	createdAt: string;
+	updatedAt: string;
+};
+
+export type AuthState = {
+	authUser: User | null;
+	isSigningUp: boolean;
+	isLoggingIn: boolean;
+	isUpdatingProfile: boolean;
+	isCheckingAuth: boolean;
+	checkAuth: () => Promise<void>;
+};
