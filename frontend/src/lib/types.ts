a795e98927c6b754af01f28636fd5d3fbceb9171
WolfY <david.skliarov.dev@gmail.com>
2026-04-08 17:49:29 +0400

    feat: add daisyui theme type

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -21,6 +21,48 @@ export type AuthState = {
 	updateProfile: (data: { profilePic: string }) => Promise<void>;
 };
 
+export type Theme =
+	| "light"
+	| "dark"
+	| "cupcake"
+	| "bumblebee"
+	| "emerald"
+	| "corporate"
+	| "synthwave"
+	| "retro"
+	| "cyberpunk"
+	| "valentine"
+	| "halloween"
+	| "garden"
+	| "forest"
+	| "aqua"
+	| "lofi"
+	| "pastel"
+	| "fantasy"
+	| "wireframe"
+	| "black"
+	| "luxury"
+	| "dracula"
+	| "cmyk"
+	| "autumn"
+	| "business"
+	| "acid"
+	| "lemonade"
+	| "night"
+	| "coffee"
+	| "winter"
+	| "dim"
+	| "nord"
+	| "sunset"
+	| "caramellatte"
+	| "abyss"
+	| "silk";
+
+export type ThemeState = {
+	theme: Theme;
+	setTheme: (theme: Theme) => void;
+};
+
 type SignUpData = {
 	fullName: string;
 	email: string;
