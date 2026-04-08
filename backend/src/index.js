d5d9aa657deb50b96623de7e23131aedc9a0e07f
WolfY <david.skliarov.dev@gmail.com>
2026-04-08 14:10:15 +0400

    refactor: increase file upliad limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,8 @@ dotenv.config();
 
 // Middleware
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
