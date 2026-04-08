161a16f522e1d992fb5db127eba87dcdbcf1314a
WolfY <david.skliarov.dev@gmail.com>
2026-04-08 14:37:41 +0400

    refactor: reduce image upload size to 5mb

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,8 @@ dotenv.config();
 
 // Middleware
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
