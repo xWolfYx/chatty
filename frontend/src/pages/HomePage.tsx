40a08b543b2b98f1449c30b13e47405190bb69eb
WolfY <david.skliarov.dev@gmail.com>
2026-04-14 17:11:22 +0400

    feat: implement base HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,20 @@
+import { useChatStore } from "../store/useChatStore";
+
 export default function HomePage() {
-	return <div>Home Page</div>;
+	const { selectedUser } = useChatStore();
+
+	return (
+		<div className="bg-base-200 h-screen">
+			<div className="flex justify-center items-center px-4 pt-20">
+				<div className="bg-base-100 shadow-xl rounded-lg w-full max-w-6xl h-[calc(100vh-8rem)]">
+					<div className="flex rounded-lg h-full overflow-hidden">
+						{/* !TODO: Create Sidebar component */}
+						<Sidebar />
+						{/* !TODO: Create components below */}
+						{!selectedUser ? <NoChatSelected /> : <Chat />}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
