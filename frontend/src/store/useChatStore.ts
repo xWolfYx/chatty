84a5a83c6870c6c18d3330569b99a112be72180b
WolfY <david.skliarov.dev@gmail.com>
2026-04-14 17:10:55 +0400

    feat: add useChatStore state

---
+++ frontend/src/store/useChatStore.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+import { create } from "zustand";
+import type { ChatState } from "../lib/types";
+
+export const useChatStore = create<ChatState>((set) => ({
+	messages: [],
+	users: [],
+	selectedUser: null,
+	isUsersLoading: false,
+	isMessagesLoading: false,
+
+	getUsers: async () => {
+		set({ isUsersLoading: true });
+		try {
+			const res = await axios.get("/messages/users");
+			set({ users: res.data });
+		} catch (error) {
+			if (axios.isAxiosError(error))
+				toast.error(error.response?.data?.message || "Failed loading users");
+			else toast.error("Something went wrong");
+		} finally {
+			set({ isUsersLoading: false });
+		}
+	},
+
+	getMessages: async (userId) => {
+		set({ isMessagesLoading: true });
+		try {
+			const res = await axios.get(`/messages/${userId}`);
+			set({ messages: res.data });
+		} catch (error) {
+			if (axios.isAxiosError(error))
+				toast.error(error.response?.data?.message || "Failed loading Messages");
+			else toast.error("Something went wrong");
+		} finally {
+			set({ isMessagesLoading: false });
+		}
+	},
+}));
