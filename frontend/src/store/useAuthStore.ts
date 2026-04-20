0b5c40cce3f148177743a27bd2d28f68f46c3255
WolfY <david.skliarov.dev@gmail.com>
2026-04-20 17:57:11 +0400

    feat: implement connectSocket function

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,21 +1,26 @@
 import axios from "axios";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import type { AuthState, User } from "../lib/types";
 
-const useAuthStore = create<AuthState>((set) => ({
+const BASE_URL = "http://localhost:5000";
+
+const useAuthStore = create<AuthState>((set, get) => ({
 	authUser: null,
 	isSigningUp: false,
 	isLoggingIn: false,
 	isUpdatingProfile: false,
 	isCheckingAuth: true,
 	onlineUsers: [],
+	socket: null,
 
 	checkAuth: async () => {
 		try {
 			const res = await axiosInstance.get<User>("/auth/check");
 			set({ authUser: res.data });
+			get().connectSocket();
 		} catch (error: unknown) {
 			if (error instanceof Error) console.log(error.message);
 			set({ authUser: null });
@@ -31,6 +36,7 @@ const useAuthStore = create<AuthState>((set) => ({
 			const res = await axiosInstance.post("/auth/signup", data);
 			set({ authUser: res.data });
 			toast.success("Account created successfully");
+			get().connectSocket();
 		} catch (error) {
 			if (axios.isAxiosError(error))
 				toast.error(error.response?.data?.message || "Signup failed");
@@ -57,6 +63,7 @@ const useAuthStore = create<AuthState>((set) => ({
 			const res = await axiosInstance.post("/auth/login", data);
 			set({ authUser: res.data });
 			toast.success("Logged in successfully");
+			get().connectSocket();
 		} catch (error) {
 			if (axios.isAxiosError(error))
 				toast.error(error.response?.data?.message || "Login failed");
@@ -82,6 +89,16 @@ const useAuthStore = create<AuthState>((set) => ({
 			set({ isUpdatingProfile: false });
 		}
 	},
+
+	connectSocket: () => {
+		const { authUser } = get();
+		if (!authUser || get().socket?.connected) return;
+
+		const socket = io(BASE_URL);
+		socket.connect();
+	},
+
+	disconnectSocket: () => {},
 }));
 
 export { useAuthStore };
